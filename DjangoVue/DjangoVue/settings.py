b8f0bb5b7f24563b04dde4003f9261d72525f1b2
huseynovfuad <55555038+huseynovfuad@users.noreply.github.com>
2020-04-19 11:36:10 +0400

    change settings

--- DjangoVue/DjangoVue/settings.py
+++ DjangoVue/DjangoVue/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_*%1br__xm!xgps*y)0zfrjo9p!9o$r+3*_xs26!q(_^7d^^*!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
